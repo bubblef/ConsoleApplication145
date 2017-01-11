a6e97eb446e5bd3b0cfc6b081280cb12bf97d1a9
Katya Kulikova <katya.kulikova@jetbrains.com>
2017-01-11 16:31:46 +0300

    Initial commit

---
+++ ConsoleApplication145/Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ConsoleApplication145{
+    internal class Program{
+        public static void Main(string[] args){
+        }
+    }
+}
